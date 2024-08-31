eca901a8cc440cdce2e93b0447aeb275fde2f564
RUK KEOSUNG <rukzeedza@gmail.com>
2024-08-31 22:44:45 +0700

    add delete user in admin page

--- app/api/totalusers/route.ts
+++ app/api/totalusers/route.ts
@@ -1,6 +1,6 @@
 import { connectMongoDB } from "@/lib/mongodb";
 import User from "@/models/user";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
 export async function GET() {
     try {
@@ -9,6 +9,21 @@ export async function GET() {
         return NextResponse.json({ totalUsers }, { status: 200 })
     } catch (err) {
         console.error(err)
+    }
+
+}
+
+export async function DELETE(req: NextRequest) {
+    const id = req.nextUrl.searchParams.get("id")
+
+    try {
+        await connectMongoDB()
+        await User.findByIdAndDelete(id)
+        return NextResponse.json({ message:"User was Deleted" }, { status: 200 })
+    } catch (err) {
+        console.error(err)
+        return NextResponse.json({ error:"cannot delete user"}, { status: 500 })
+
     }
 
 }
